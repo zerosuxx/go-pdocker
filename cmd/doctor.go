ab7d2420ae600d9bf9814ef8dd71c8967e0c2a86
Tamas Mohos <zerosuxx@gmail.com>
2021-09-23 02:39:05 +0200

    refactor(doctor): extract duplicated codes to printRequirementResult method

--- cmd/doctor.go
+++ cmd/doctor.go
@@ -9,40 +9,63 @@ import (
 )
 
 var doctorCmd = &cobra.Command{
-	Use:   	"doctor",
-	Short: 	"Check system requirements",
-	Run: 	runDoctor,
+	Use:   "doctor",
+	Short: "Check system requirements",
+	Run:   runDoctor,
 }
 
 func runDoctor(_ *cobra.Command, _ []string) {
 	prootCommand := exec.Command("proot", "--help")
 	prootOutput, prootError := prootCommand.Output()
-
-	if prootError != nil {
-		fmt.Println("[ ] PRoot is not installed (See: https://proot-me.github.io)")
-	} else {
-		version := strings.TrimLeft(strings.Split(string(prootOutput), ":")[0], "proot ")
-		fmt.Println(fmt.Sprintf("[X] PRoot (%s) installed", version))
-	}
+	printRequirementResult("proot is not installed (See: https://proot-me.github.io)", prootOutput, prootError, func(output string) string {
+		version := strings.TrimLeft(strings.Split(output, ":")[0], "proot ")
+		return fmt.Sprintf("proot (%s) installed", version)
+	})
 
 	bashCommand := exec.Command("bash", "--version")
 	bashOutput, bashError := bashCommand.Output()
 
-	if bashError != nil {
-		fmt.Println("[ ] Bash is not installed")
-	} else {
-		firstLine := strings.Split(string(bashOutput), "\n")[0]
-		version := strings.Split(firstLine, "version ")[1]
-		fmt.Println(fmt.Sprintf("[X] Bash (%s) installed", version))
-	}
+	printRequirementResult("bash is not installed", bashOutput, bashError, func(output string) string {
+		firstLine := strings.Split(output, "\n")[0]
+		version := strings.Split(firstLine, " ")[3]
+		return fmt.Sprintf("bash (%s) installed", version)
+	})
 
-	if _, err := os.Stat(imagePullScriptPath); err != nil {
-		fmt.Println("[ ] Pull script is not installed")
-	} else {
-		fmt.Println(fmt.Sprintf("[X] Pull script (%s) installed", imagePullScriptPath))
-	}
+	curlCommand := exec.Command("curl", "--version")
+	curlOutput, curlError := curlCommand.Output()
+
+	printRequirementResult("curl is not installed", curlOutput, curlError, func(output string) string {
+		firstLine := strings.Split(output, "\n")[0]
+		version := strings.Split(firstLine, " ")[1]
+		return fmt.Sprintf("curl (%s) installed", version)
+	})
+
+	jqCommand := exec.Command("jq", "--version")
+	jqOutput, jqError := jqCommand.Output()
+
+	printRequirementResult("jq is not installed", jqOutput, jqError, func(output string) string {
+		version := strings.Trim(output, "\n")
+		return fmt.Sprintf("jq (%s) installed", version)
+	})
+
+	_, imagePullScriptError := os.Stat(imagePullScriptPath)
+	printRequirementResult("image pull script is not installed", []byte{}, imagePullScriptError, func(_ string) string {
+		return fmt.Sprintf("image pull script (%s) installed", imagePullScriptPath)
+	})
+
 }
 
 func init() {
 	rootCmd.AddCommand(doctorCmd)
+}
+
+type successTextTransformer func(string) string
+
+func printRequirementResult(errorText string, output []byte, err error, transformer successTextTransformer) {
+	if err != nil {
+		fmt.Println("[ ] " + errorText)
+	} else {
+		successText := transformer(string(output))
+		fmt.Println(fmt.Sprintf("[X] " + successText))
+	}
 }
