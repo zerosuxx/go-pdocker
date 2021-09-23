ca960f13ff4f4b7082ceb4afff6631941ba3d478
Tamas Mohos <zerosuxx@gmail.com>
2021-09-23 20:26:06 +0200

    feat(tar_extractor): add ExtractTarArchive method

---
+++ pkg/tar_extractor.go
@@ -0,0 +1,66 @@
+package pkg
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func ExtractTarArchive(r io.Reader, targetPath string) error {
+	gzr, err := gzip.NewReader(r)
+	if err != nil {
+		return err
+	}
+	defer func(gzr *gzip.Reader) {
+		_ = gzr.Close()
+	}(gzr)
+
+	tr := tar.NewReader(gzr)
+
+	for {
+		header, err := tr.Next()
+
+		switch {
+		case err == io.EOF:
+			return nil
+
+		case err != nil:
+			return err
+
+		case header == nil:
+			continue
+		}
+
+		target := filepath.Join(targetPath, header.Name)
+		typeSymLink := byte(50)
+
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if _, err := os.Stat(target); err != nil {
+				if err := os.MkdirAll(target, 0755); err != nil {
+					return err
+				}
+			}
+
+		case tar.TypeReg:
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return err
+			}
+
+			if _, err := io.Copy(f, tr); err != nil {
+				return err
+			}
+
+			_ = f.Close()
+
+		case typeSymLink:
+			err := os.Symlink(filepath.Join(targetPath, header.Linkname), target)
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
